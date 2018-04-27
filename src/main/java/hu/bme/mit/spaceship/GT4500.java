0d55eb3755fa2e68c436e9815d0123f3857bc88c
Martin Borbola <borbolam14@gmail.com>
2018-04-26 23:54:16 -0700

    Code smell resolved with switch replacement with if

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -38,8 +38,7 @@ public class GT4500 implements SpaceShip {
 
     boolean firingSuccess = false;
 
-    switch (firingMode) {
-      case SINGLE:
+    if (firingMode == SINGLE) {
         if (wasPrimaryFiredLast) {
           // try to fire the secondary first
           if (! secondaryTorpedoStore.isEmpty()) {
@@ -74,9 +73,8 @@ public class GT4500 implements SpaceShip {
             // if both of the stores are empty, nothing can be done, return failure
           }
         }
-        break;
-
-      case ALL:
+	}
+      else if(firingMode == ALL) {
         // try to fire both of the torpedo stores
 	boolean first = false;
 	boolean second = false;
@@ -87,9 +85,8 @@ public class GT4500 implements SpaceShip {
             second = secondaryTorpedoStore.fire(1);
           }
         firingSuccess = first && second;
-
-        break;
     }
+}
 
     return firingSuccess;
   }
